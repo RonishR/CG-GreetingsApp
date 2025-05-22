a56e2ec67fee9861ae762eb1669a3c4333b5173e
Ronish Raja <ronishraja1@gmail.com>
2025-05-22 15:48:53 +0530

    Added GET endpoint to return personalized greeting using query parameter

--- src/main/java/com/example/greetingsapp/controller/HelloController.java
+++ src/main/java/com/example/greetingsapp/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.greetingsapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,10 @@ public class HelloController {
 	public String sayhello() {
 		return "Hello from BridgeLabz";
 	}
+	
+	@GetMapping("/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
 
 }
