077723496773b49a40c7ebcf7e8ae1b626d3ae97
Ronish Raja <ronishraja1@gmail.com>
2025-05-22 16:16:35 +0530

    Added PUT endpoint to return greeting using path variable and query parameter

--- src/main/java/com/example/greetingsapp/controller/HelloController.java
+++ src/main/java/com/example/greetingsapp/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.greetingsapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,5 +34,11 @@ public class HelloController {
     public String sayHelloPost(@RequestBody UserDTO user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
+	
+	@PutMapping("/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
+
 
 }
