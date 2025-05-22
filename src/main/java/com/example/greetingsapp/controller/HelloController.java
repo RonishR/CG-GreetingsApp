ad15744a29880c4e1d326bec70b2a2d6ce75fa53
Ronish Raja <ronishraja1@gmail.com>
2025-05-22 15:54:19 +0530

    Add GET endpoint to return personalized greeting using path variable

--- src/main/java/com/example/greetingsapp/controller/HelloController.java
+++ src/main/java/com/example/greetingsapp/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.greetingsapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,11 @@ public class HelloController {
 	
 	@GetMapping("/query")
     public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+	
+	@GetMapping("/param/{name}")
+    public String sayHelloPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 
