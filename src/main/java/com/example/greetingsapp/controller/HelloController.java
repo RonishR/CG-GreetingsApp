6609da4b405afed6645d49bad5dd4e79694cddf5
Ronish Raja <ronishraja1@gmail.com>
2025-05-22 16:08:07 +0530

    Added POST endpoint to return greeting using JSON request body (firstName, lastName)

--- src/main/java/com/example/greetingsapp/controller/HelloController.java
+++ src/main/java/com/example/greetingsapp/controller/HelloController.java
@@ -2,9 +2,13 @@ package com.example.greetingsapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.greetingsapp.dto.UserDTO;
 
 @RestController
 @RequestMapping("/hello")
@@ -23,6 +27,11 @@ public class HelloController {
 	@GetMapping("/param/{name}")
     public String sayHelloPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
+    }
+	
+	@PostMapping("/post")
+    public String sayHelloPost(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
 
 }
